6c4e9f8f4755e7e17a427c0949d060a8c2118aea
Amanda Ramirez <amandadeog@gmail.com>
2022-04-14 06:02:32 -0300

    add atualizações ao repositório

---
+++ assets/script/menu-mobile.js
@@ -0,0 +1,27 @@
+const botaoMobile = document.querySelector('.menu__mobile__botao');
+const menuLinks = document.querySelector('.menu__mobile');
+const closeMenu = document.querySelector('.menu__mobile__close')
+const body = document.querySelector('body')
+
+const exibeMenuMobile =  () => {
+   menuLinks.style.display = "block";
+   body.style.backgroundColor = "#494949ce";
+   botaoMobile.style.display ="none";
+   closeMenu.style.display = "block";
+}
+
+const fechaMenu = () => {
+   menuLinks.style.display = "none";
+   body.style.backgroundColor = "transparent";
+   botaoMobile.style.display ="block";
+   closeMenu.style.display = "none";
+}
+
+closeMenu.onclick = function () {
+   fechaMenu()
+}
+
+botaoMobile.onclick = function () {
+   exibeMenuMobile();
+   console.log("Botao clicado")
+}
